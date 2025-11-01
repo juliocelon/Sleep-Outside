0f5e81f55b19bc3d49666a23ca8971cf4d9ef844
jkkitchen <jkitchen1@byupathway.edu>
2025-11-01 15:00:55 -0400

    Fixed cart problems

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,9 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cart = getLocalStorage("so-cart") || []; //pulls existing cart or creates an empty array to hold items added to the cart
+  cart.push(product); //adds the new product to the cart
+  setLocalStorage("so-cart", cart); //updates the local storage with the new value added to the cart
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
