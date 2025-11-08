0e6d86cef50b16738e5bf6f7710423055f9239d9
Julio Cesar Celon Altamirano <juliocelon@ibm.com>
2025-11-08 07:23:53 -0600

    fixing comments

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -13,15 +13,12 @@ function productCardTemplate(product) {
 
 export default class ProductList {
     constructor(category, dataSource, listElement) {
-        // You passed in this information to make the class as reusable as possible.
-        // Being able to define these things when you use the class will make it very flexible
         this.category = category;
         this.dataSource = dataSource;
         this.listElement = listElement;
     }
 
     async init() {
-        // the dataSource will return a Promise...so you can use await to resolve it.
         const list = await this.dataSource.getData();
         this.renderList(list);
     }
