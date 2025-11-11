349ee6e47c6754d684073c3f1673060e5c305b2f
jkkitchen <jkitchen1@byupathway.edu>
2025-11-10 20:46:06 -0500

    fixed superscript

--- src/js/cartSuperscript.js
+++ src/js/cartSuperscript.js
@@ -4,10 +4,16 @@ function cartSuperscript() {
     //Pull items in cart from local storage
     const cartItems = getLocalStorage('so-cart');
     //Count the number of items in the cart
-    const numberItemsInCart = cartItems.length;
+    let numberItemsInCart;
+    if (cartItems) {
+        numberItemsInCart = cartItems.length;
+    } else {
+        numberItemsInCart = 0
+    }
+    
 
     //Check to see if anything is in the cart
-    if (numberItemsInCart) { //means if value is NOT falsy (or in other words, is not false, null, 0, etc.)
+    if (numberItemsInCart>0) { //means if value is NOT falsy (or in other words, is not false, null, 0, etc.)
         //Pull the superscript element from all the index.html's and remove the class list 'hide' so it will show
         const superscript = document.querySelector('.superscript');
         superscript.classList.remove('hide');
