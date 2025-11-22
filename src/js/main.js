1678c5b5c003ce069e573ff5cf8a4af40ceceeae
jkkitchen <jkitchen1@byupathway.edu>
2025-11-22 15:58:09 -0500

    Added newsletter sign-up to main page

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,49 @@ loadHeaderFooter();
 //Function to generate an alert on the home page
 const alert = new Alert();
 alert.createAlerts();
+
+//Newsletter Sign-Up
+//Add event listener so the modal will pop up when submit is clicked
+const newsletterDialog = document.querySelector("#newsletter-modal");
+
+document.querySelector("#newsletter-submit").addEventListener("click", (event) => {
+    event.preventDefault(); //prevents page refresh
+    const newsletterForm = document.querySelector('#newsletter-form');
+    const chk_status = newsletterForm.checkValidity();
+    newsletterForm.reportValidity();
+    if (chk_status) {
+        //Save name and email to local storage
+        const firstName = newsletterForm.fname.value;
+        const lastName = newsletterForm.lname.value;
+        const email = newsletterForm.userEmail.value;
+
+        addSubscriber(firstName, lastName, email);
+
+        //Show modal
+        newsletterDialog.showModal();
+
+        //Clear form
+        newsletterForm.reset();
+    }
+});
+
+//Add event listener to close the modal
+const newsletterModalClose = document.querySelector("#close-modal");
+newsletterModalClose.addEventListener("click", () => {
+    newsletterDialog.close();
+})
+
+//Function to save subscriber to local Storage
+function addSubscriber(firstName, lastName, email) {
+    //Pull subscribers array from local storage
+    const newsletterSubscribers = JSON.parse(localStorage.getItem("newsletterSubscribers")) || [];
+
+    //Add new subscriber
+    const newSubscriber = { firstName, lastName, email };
+
+    //Add new subscriber to array
+    newsletterSubscribers.push(newSubscriber);
+
+    //Save updated array back to localStorage
+    localStorage.setItem("newsletterSubscribers", JSON.stringify(newsletterSubscribers));
+}
